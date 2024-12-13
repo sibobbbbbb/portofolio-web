e3424a71dc605a8a2160ff7b11ef6a2382073d5d
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-12-13 22:56:57 +0700

    feat: add speed insight vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
